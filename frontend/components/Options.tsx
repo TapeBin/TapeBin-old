24150f53170c4b0298b0cbfb98bd04ab67476087
Bunyamin Duduk <bunyamin.duduk@hva.nl>
2021-06-21 01:02:25 +0200

    Added the opening of the bin options for mobile

--- frontend/components/Options.tsx
+++ frontend/components/Options.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { Box } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Box, Button, Stack } from "@chakra-ui/react";
 
 const Options = () => {
-
+    const [isOpen, setOpen] = useState(false);
     return (
         <>
             <Box
@@ -21,10 +21,20 @@ const Options = () => {
             <Box
                 display={["block", "block", "block", "block", "none"]}
                 w="100%"
-                h="40px"
+                h={isOpen ? "300px" : "40px"}
                 bg="red"
             >
+                <Box
+                    display={isOpen ? "block" : "none"}
+                >
+                    <Stack alignItems="center" alignContent="center">
+                        <div>asdasdasd</div>
+                    </Stack>
 
+
+                </Box>
+
+                <Button onClick={() => setOpen(!isOpen)}>{isOpen ? "Close" : "Open"}</Button>
             </Box>
         </>
     );
