1933f3cbdab8da9131ed792d7a9ef921bf2328b9
Bunyamin Duduk <bunyamin.duduk@hva.nl>
2021-06-24 21:45:50 +0200

    Dockerized

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,10 @@ import express from "express";
 const app = express();
 const PORT = 5000;
 
+app.get("/", (req, res) => {
+    res.send("Server is working!");
+})
+
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
 });
