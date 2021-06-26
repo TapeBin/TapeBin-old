9d5506bb9988dbec0bd2aba298e60b0d504bc19f
Bunyamin Duduk <bunyamin.duduk@hva.nl>
2021-06-26 21:02:13 +0200

    Differentiated the things

--- frontend/components/Profile.tsx
+++ frontend/components/Profile.tsx
@@ -27,7 +27,7 @@ const Profile: FunctionComponent<ProfileProps> = (props: ProfileProps) => {
             <DrawerOverlay/>
             <DrawerContent bg={"#232323"} color="white" alignItems={"center"}>
                 <DrawerBody mt="5em" overflow={"hidden"} h="100%" w="100%">
-                    <Flex justifyContent="flex-start" ml={{base: "1em", xl: "5em"}} mt={{base: "0.7em", xl: "2em"}}>
+                    <Flex justifyContent="flex-start" ml={{ base: "1em", xl: "5em" }} mt={{ base: "0.7em", xl: "2em" }}>
                         <Button
                             borderRadius={3}
                             border="3px solid rgba(68, 68, 68, 1)"
@@ -45,48 +45,53 @@ const Profile: FunctionComponent<ProfileProps> = (props: ProfileProps) => {
                         </Button>
                     </Flex>
 
-                    <Box w={{base: "95%", xl: "80%"}} m="0 auto" mt={"2em"} borderBottom="1px solid rgb(126, 126, 126)" pb="20px">
-                        <Flex justifyContent="space-between">
-                            <Stack direction="row" spacing={{base: "0.7em", xl: "1.6em"}} alignItems={"center"}>
-                                <Image src={user.profileImage} height={{base: "50px", xl: "70px"}} borderRadius="50%"/>
-                                <Stack>
-                                    <Flex direction="row" alignItems={"center"} w={"100%"}>
-                                        <Text fontFamily="Inter" fontSize={{base: "20px", xl: "30px"}}>{user.username}</Text>
-                                        <Image src="githubIcon.svg" h="25px" ml="10px"/>
-                                    </Flex>
-                                    <Stack direction="row" fontFamily="Poppins" textAlign="center">
-                                        <Text
-                                            w={{base: "50px", xl: "60px"}}
-                                            fontSize={{base: "13px", xl: "16px"}}
-                                            bgColor={user.isPro ? "#00C2FF" : "rgba(68, 68, 68, 0.59)"}
-                                            borderRadius="4px"
-                                            p={{base: "2px 1px", xl: "1px 5px"}}
-                                        >FREE</Text>
-                                        <Text color="rgb(205,205,205)" fontSize={{base: "13px", xl: "16px"}}>
-                                            Permanent
-                                        </Text>
-                                    </Stack>
+                    <Stack w={{ base: "95%", xl: "80%" }} m="0 auto" mt={"2em"} >
+                        <Box borderBottom="1px solid rgb(126, 126, 126)" pb="20px">
+                            <Flex justifyContent="space-between">
+                                <Stack direction="row" spacing={{ base: "0.7em", xl: "1.6em" }} alignItems={"center"}>
+                                    <Image src={user.profileImage} height={{ base: "50px", xl: "70px" }}
+                                           borderRadius="50%"/>
+                                    <Stack>
+                                        <Flex direction="row" alignItems={"center"} w={"100%"}>
+                                            <Text fontFamily="Inter"
+                                                  fontSize={{ base: "20px", xl: "30px" }}>{user.username}</Text>
+                                            <Image src="githubIcon.svg" h="25px" ml="10px"/>
+                                        </Flex>
+                                        <Stack direction="row" fontFamily="Poppins" textAlign="center">
+                                            <Text
+                                                w={{ base: "50px", xl: "60px" }}
+                                                fontSize={{ base: "13px", xl: "16px" }}
+                                                bgColor={user.isPro ? "#00C2FF" : "rgba(68, 68, 68, 0.59)"}
+                                                borderRadius="4px"
+                                                p={{ base: "2px 1px", xl: "1px 5px" }}
+                                            >FREE</Text>
+                                            <Text color="rgb(205,205,205)" fontSize={{ base: "13px", xl: "16px" }}>
+                                                Permanent
+                                            </Text>
+                                        </Stack>
 
+                                    </Stack>
                                 </Stack>
-                            </Stack>
-                            <Button
-                                borderRadius={3}
-                                border="3px solid transparent"
-                                backgroundColor="rgba(68, 68, 68, 1)"
-                                paddingX={[4, 7]}
-                                bg="transparent"
-                                transition="none"
-                                fontWeight="regular"
-                                alignSelf="center"
-                                fontSize={[12, 16]}
-                                _hover={{ bg: "transparent", border: "3px solid rgba(68, 68, 68, 1)" }}
-                                _active={{ bg: "transparent", border: "3px solid rgba(68, 68, 68, 1)" }}
-                                _focus={{ border: "3px solid transparent" }}
-                            >
-                                Logout
-                            </Button>
-                        </Flex>
-                    </Box>
+                                <Button
+                                    borderRadius={3}
+                                    border="3px solid transparent"
+                                    backgroundColor="rgba(68, 68, 68, 1)"
+                                    paddingX={[4, 7]}
+                                    bg="transparent"
+                                    transition="none"
+                                    fontWeight="regular"
+                                    alignSelf="center"
+                                    fontSize={[12, 16]}
+                                    _hover={{ bg: "transparent", border: "3px solid rgba(68, 68, 68, 1)" }}
+                                    _active={{ bg: "transparent", border: "3px solid rgba(68, 68, 68, 1)" }}
+                                    _focus={{ border: "3px solid transparent" }}
+                                >
+                                    Logout
+                                </Button>
+                            </Flex>
+                        </Box>
+                    </Stack>
+
 
                 </DrawerBody>
             </DrawerContent>
